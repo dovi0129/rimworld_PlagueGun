2efe489161c3941e695edcaee990f8ff72109aea
DOVAHKIIN404 <bmw5779@gmail.com>
2024-08-11 00:41:10 +0900

    Initial commit with all project files

---
+++ PlagueGun/YourLiking.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+
+namespace TST_PlagueGun
+{
+    public class ModExtension_PlagueBullet : DefModExtension
+    {
+        public float addHediffChance = 0.1f;
+        public HediffDef hediffToAdd;
+    }
+}
